07926dcd7d34eb6983ad41c417b99ff2fdc16479
ahsenur <ahsenm050@gmail.com>
2026-03-27 23:30:07 +0300

    Update radar_filtresi.cpp

--- radar_filtresi.cpp
+++ radar_filtresi.cpp
@@ -1,25 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/msg/pose.hpp"
+#include <cmath>
 
 class RadarFiltresi : public rclcpp::Node {
 public:
     RadarFiltresi() : Node("radar_filtresi_node") {
-        subscription_ = this->create_subscription<turtlesim::msg::Pose>(
-            "turtle1/pose", 10, std::bind(&RadarFiltresi::pose_callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "İNÜFEST: C++ Radar Filtresi Aktif (Yüksek Hız Modu).");
+        sub2_ = this->create_subscription<turtlesim::msg::Pose>("turtle2/pose", 10, std::bind(&RadarFiltresi::analiz, this, std::placeholders::_1));
+        sub3_ = this->create_subscription<turtlesim::msg::Pose>("turtle3/pose", 10, std::bind(&RadarFiltresi::analiz, this, std::placeholders::_1));
+        sub4_ = this->create_subscription<turtlesim::msg::Pose>("turtle4/pose", 10, std::bind(&RadarFiltresi::analiz, this, std::placeholders::_1));
     }
-
 private:
-    void pose_callback(const turtlesim::msg::Pose::SharedPtr msg) const {
-        // C++ burada radar verilerini işliyor
-        if (msg->x > 7.0 || msg->y > 3.0) {
-            RCLCPP_WARN(this->get_logger(), "KRİTİK: İHA Sınırda! Konum: X=%.2f, Y=%.2f", msg->x, msg->y);
+    void analiz(const turtlesim::msg::Pose::SharedPtr msg) {
+        double delta = 4 * pow(2.0, 2) * pow(1.5, 2) * (pow(msg->y - tan(msg->theta) * msg->x, 2) + pow(1.5, 2) - pow(2.0, 2) * pow(tan(msg->theta), 2));
+        if (delta > 0) {
+            RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 500, "!!! RISK ALGILANDI !!! Delta: %.2f", delta);
         }
     }
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr sub2_, sub3_, sub4_;
 };
 
-int main(int argc, char * argv[]) {
+int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<RadarFiltresi>());
     rclcpp::shutdown();
